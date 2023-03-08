08cd316c29db3254eaadf72231e3325dc9dc051f
Mike <hgelhem@gmail.com>
2023-03-08 13:38:20 +0300

    сделал интерфейс UserService расширяющим UserDao

--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,15 +1,23 @@
 package hiber.service;
 
+import hiber.dao.UserDao;
 import hiber.model.Car;
 import hiber.model.User;
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDao {
+    @Override
     void add(User user);
 
+    @Override
     void add(User user, Car car);
 
+    @Override
     List<User> listUsers();
 
+    @Override
     List<Car> listCars();
+
+    @Override
+    User getUserByCar(String model, int series);
 }
