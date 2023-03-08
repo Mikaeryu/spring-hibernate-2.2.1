7eb07826c3adfb71acd7b2e9757ab671ccfc9dfd
Mike <hgelhem@gmail.com>
2023-03-08 11:05:49 +0300

    сделал код для тестирования в мейне

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,15 +11,46 @@ import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) throws SQLException {
+//        AnnotationConfigApplicationContext context =
+//                new AnnotationConfigApplicationContext(AppConfig.class);
+//
+//        UserService userService = context.getBean(UserService.class);
+//
+//        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//
+//        List<User> users = userService.listUsers();
+//        for (User user : users) {
+//            System.out.println("Id = " + user.getId());
+//            System.out.println("First Name = " + user.getFirstName());
+//            System.out.println("Last Name = " + user.getLastName());
+//            System.out.println("Email = " + user.getEmail());
+//            System.out.println();
+//        }
+//
+//        context.close();
+
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        new AnnotationConfigApplicationContext(AppConfig.class);
 
         UserService userService = context.getBean(UserService.class);
 
-        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+        Car car1 = new Car("Nissan", 1234);
+        Car car2 = new Car("Toyota", 2345);
+        Car car3 = new Car("Mazda", 3456);
+        Car car4 = new Car("Lexus", 4567);
+
+        userService.add(user1, car1);
+        userService.add(user2, car2);
+        userService.add(user3, car3);
+        userService.add(user4, car4);
 
         List<User> users = userService.listUsers();
         for (User user : users) {
@@ -30,5 +62,6 @@ public class MainApp {
         }
 
         context.close();
+
     }
 }
