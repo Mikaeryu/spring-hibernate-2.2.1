16a482f4cdec264ff204124a616390c481af2c2a
Mike <hgelhem@gmail.com>
2023-03-09 10:36:49 +0300

    Добавил комменты в UserDaoImp

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -21,12 +21,14 @@ public class UserDaoImp implements UserDao {
         sessionFactory.getCurrentSession().save(user);
     }
 
+    //Добавил перегруженный метод для добавления юзера вместе с с машиной
     @Override
     public void add(User user, Car car) {
         user.setCar(car);
         sessionFactory.getCurrentSession().save(user);
     }
 
+    //метод для поиска юзера по машине
     @Override
     @SuppressWarnings("unchecked")
     public User getUserByCar(String model, int series) {
@@ -39,7 +41,7 @@ public class UserDaoImp implements UserDao {
         if (!carFoundOptional.isPresent()) {
             System.err.println("There is no car witch such parameters");
             return null;
-        }
+        } //вот тут вообще не уверен насчёт выкидывание null, это нормально, так делать? просто это рядовой запрос на поиск
 
         Car carFound = carFoundOptional.get();
         long userId = carFound.getId();
@@ -56,6 +58,7 @@ public class UserDaoImp implements UserDao {
         return query.getResultList();
     }
 
+    //добавил метод, возрвращающий список машин
     @Override
     @SuppressWarnings("unchecked")
     public List<Car> listCars() {
